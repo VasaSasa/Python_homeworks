1e968f7d4aafc69bff9e199c5a899901a5f20551
Václav Doležal <bc.va.do@gmail.com>
2022-04-14 13:06:59 +0200

    created 3 task of Python and rewrite it to index.html, some style adjucement

---
+++ python01.py
@@ -0,0 +1,93 @@
+
+
+#for i in range(1,11):
+    #print(i)
+
+
+
+#for i in range(5,16):
+    #print(i)
+
+
+
+#for i in range(7):
+ #   print("ahoj")
+
+
+#for i in range(7):
+   # print(i)
+
+
+#x = [x for x in range(1,21,2)]
+#print(x)
+
+
+#for i in range(1,21,2):
+    #print(i)
+    
+#for i in range(0,21,2):
+ #   print(i)
+
+#for i in range(10,-11,-1):
+ #   print(i)
+
+
+
+#for i in range(10,0,-1):
+    #print(i)
+
+#suda_cisla = []
+#licha_cisla = []
+
+#for i in range(0,21):
+ #   if i % 2 == 0:
+  #      suda_cisla.append(i)
+   # else:
+   #     licha_cisla.append(i)
+#task 1
+#print(suda_cisla)
+#print(licha_cisla)
+
+#number_1 = int(input("Enter number one: "))
+#number_2 = int(input("Enter number two: "))
+
+#if number_1 < number_2:
+    #for i in range(number_1,number_2 + 1):
+        #print(i)#
+#elif number_1 > number_2:
+    #for i in range(number_2,number_1 + 1):
+        #print(i)
+#else:
+    #print("Entered numbers are equal each other!!!")
+
+#task 2 and 3 
+
+number_1 = int(input("Enter number one: "))
+number_2 = int(input("Enter number two: "))
+
+even_numbers = []
+odd_numbers = []
+
+for number in range(number_1, number_2 + 1):
+    if number % 2 == 0:
+        even_numbers.append(number)
+    else:
+        odd_numbers.append(number)
+
+print("Even numbers are:", even_numbers)
+print("Odd numbers are:", odd_numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
-
+/*
 let greeting;
 
 greeting = prompt("Enter your greeting of the day: ")
 
 console.log(greeting)
+*/
+
 
