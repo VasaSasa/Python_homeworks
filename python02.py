e0feee5925a3b70fcd7fe20692c7b155a87d814f
Václav Doležal <bc.va.do@gmail.com>
2022-04-15 21:46:40 +0200

    created file python02.py "how to use pass and continue"

---
+++ python02.py
@@ -0,0 +1,18 @@
+
+
+
+for i in  range(10,16):
+    print("dostaneš peníze")
+    print("dostaneš chleba")
+    if i == 12:
+        print("nedostaneš AAA ani BBB")
+        continue
+    print("dostaneš AAA")
+    print("dostaneš BBB")
+
+
+for i in range(10):
+    pass
+
+
+print("Ahoj")
